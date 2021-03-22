76bddc7af52dd7e4977f9811dc53938969c928bd
Dominique Sakrisson <domsakrisson@gmailcom>
2021-03-22 14:56:27 -0700

    capitolizeAndFilter refactor again passes

--- utils.js
+++ utils.js
@@ -4,8 +4,7 @@ const copyAndPush = (arr, newInt) => ([...arr, newInt]);
 
 const capitalizeAndFilter = (arr) => {
   const filtered = arr.filter(itemStr => itemStr.substring(0, 1).toUpperCase() !== 'F');
-  const result = filtered.map(str => str.toUpperCase());
-  return result;
+  return filtered.map(str => str.toUpperCase());
 };
 
 module.exports = { getName, copyAndPush, capitalizeAndFilter };
