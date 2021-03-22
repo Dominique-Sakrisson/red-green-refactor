fe1822827ec7703d751d2360815956b9479c6265
Dominique Sakrisson <domsakrisson@gmailcom>
2021-03-22 14:55:36 -0700

    capitolizeAndFilter refactor

--- utils.js
+++ utils.js
@@ -4,9 +4,7 @@ const copyAndPush = (arr, newInt) => ([...arr, newInt]);
 
 const capitalizeAndFilter = (arr) => {
   const filtered = arr.filter(itemStr => itemStr.substring(0, 1).toUpperCase() !== 'F');
-  const result = filtered.map(str => {
-    return str.toUpperCase();
-  });
+  const result = filtered.map(str => str.toUpperCase());
   return result;
 };
 
